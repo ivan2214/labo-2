f6bc9ca1f2c7e251175101cdfece1b66fbec0709
ivan2214 <bongiovanniivan12@gmail.com>
2024-09-20 14:34:38 -0300

    feat(navbar): Enable navigation to Opcional A
    Enables the navigation link for "Opcional A" in the navbar.

--- TP04-BongiovanniIvanAgustin/Ejercicio-05/php/encabezado.php
+++ TP04-BongiovanniIvanAgustin/Ejercicio-05/php/encabezado.php
@@ -42,7 +42,7 @@
                         Ejercicio 05
                     </a>
                 </li>
-               <!--  <li class="transition-all duration-500 hover:text-blue-500">
+                <li class="transition-all duration-500 hover:text-blue-500">
                     <a href="http://localhost/Laboratorio-2/TP04-BongiovanniIvanAgustin/Ejercicio-Opcionales-A">
                         Opcional A
                     </a>
@@ -53,13 +53,8 @@
                     </a>
                 </li>
 
-                <li class="transition-all duration-500 hover:text-blue-500">
-                    <a href="http://localhost/Laboratorio-2/TP04-BongiovanniIvanAgustin/Ejercicio-Opcionales-C">
-                        Opcional C
-                    </a>
-                </li> -->
 
             </ul>
         </nav>
-        <h1 class="text-4xl font-bold"></h1>
+        <h1 class="text-4xl font-bold">Login</h1>
     </header>
