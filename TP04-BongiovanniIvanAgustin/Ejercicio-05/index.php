c09cf82c71aaad9c179823891cea1fdc01be4555
ivan2214 <bongiovanniivan12@gmail.com>
2024-09-20 00:39:23 -0300

    feat: Scaffold basic project structure
    Initial setup of files and directories for the project.

---
+++ TP04-BongiovanniIvanAgustin/Ejercicio-05/index.php
@@ -0,0 +1,19 @@
+<?php require_once('php/encabezado.php'); ?>
+
+
+<?php
+
+?>
+
+
+
+<main class="flex justify-center h-full items-center">
+  <section class="my-5 border-2 border-gray-300 rounded-lg bg-gray-100">
+    <article class="p-4 ">
+
+    </article>
+
+  </section>
+</main>
+
+<?php require_once('php/pie.php'); ?>
