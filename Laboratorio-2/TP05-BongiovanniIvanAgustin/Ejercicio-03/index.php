717124a0a45baf493892e7427a784429a4ae2266
ivan2214 <bongiovanniivan12@gmail.com>
2024-10-03 21:13:48 -0300

    feat: Implement employee earnings report
    Added new functionality to calculate and display employee earnings.

--- Laboratorio-2/TP05-BongiovanniIvanAgustin/Ejercicio-03/index.php
+++ Laboratorio-2/TP05-BongiovanniIvanAgustin/Ejercicio-03/index.php
@@ -9,40 +9,21 @@
 
 <main class="flex justify-center h-full items-center">
   <article class="bg-white rounded-lg shadow-md p-6 w-full max-w-md">
-    <h1 class="text-2xl font-bold text-center mb-4">Financiera</h1>
-    <form action="php/ganancias.php" method="post">
-      <label for="deposito" class="block mb-2 font-medium">Depósito Inicial:</label>
-      <input 
-        type="number" 
-        id="deposito" 
-        name="deposito" 
-        class="w-full p-2 border border-gray-300 rounded mb-4" 
-        placeholder="Ingrese su depósito inicial" 
-        required 
-      />
+    <h2 class="text-2xl font-bold text-center mb-4">Consultar Ganancia</h2>
+    <form action="php/consultar.php" method="post">
+      <label for="nombre" class="block mb-2 font-medium">Nombre:</label>
+      <input
+        type="text"
+        id="nombre"
+        name="nombre"
+        class="w-full p-2 border border-gray-300 rounded mb-4"
+        placeholder="Ingrese su depósito inicial"
+        required />
 
-      <fieldset class="mb-4">
-        <legend class="font-medium">Plazo:</legend>
-        <label class="block">
-          <input type="radio" name="plazo" value="30" class="mr-2" />
-          30 días
-        </label>
-        <label class="block">
-          <input type="radio" name="plazo" value="45" class="mr-2" />
-          45 días
-        </label>
-        <label class="block">
-          <input type="radio" name="plazo" value="60" class="mr-2" />
-          60 días
-        </label>
-        <label class="block">
-          <input type="radio" name="plazo" value="90" class="mr-2" />
-          90 días
-        </label>
-      </fieldset>
+
 
       <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600">
-        Simular
+        Consultar
       </button>
     </form>
   </article>
