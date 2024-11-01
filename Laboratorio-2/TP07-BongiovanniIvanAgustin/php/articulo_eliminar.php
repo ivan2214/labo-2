1eee159d591cde5e4a1465acf4acb3199db478e4
ivan2214 <bongiovanniivan12@gmail.com>
2024-10-31 22:57:50 -0300

    fix: Enhance article deletion process
    Improve DB interaction and error handling.

--- Laboratorio-2/TP07-BongiovanniIvanAgustin/php/articulo_eliminar.php
+++ Laboratorio-2/TP07-BongiovanniIvanAgustin/php/articulo_eliminar.php
@@ -4,8 +4,10 @@ require("encabezado.php");
 
 
 if (!empty($_GET["id"]) && !empty($_GET["usuario"])) {
+    // hago el casteo de $_GET["id"] a int pq een la db es int
     $id = (int) $_GET["id"];
     $usuario = $_GET["usuario"];
+    // declaro la variable para saber si fue eliminado o no el articulo
     $eliminado = false;
 
     require_once 'conexion.php';
@@ -13,23 +15,27 @@ if (!empty($_GET["id"]) && !empty($_GET["usuario"])) {
     $conexion = conectar();
 
     if (!$conexion) {
+        // si no se ha podido conectar con la base de datos mostrar el error y redirigir
         header("refresh:1;url=../index.php");
         echo '<p>No se ha podido conectar con la base de datos</p>';
     } else {
+        // creo la consulta
         $consulta = "SELECT nombre FROM articulo WHERE id_articulo = ? ";
-
+        // preparo la sentencia
         $sentencia = mysqli_prepare($conexion, $consulta);
-
+        // asigno los valores a la sentencia
         mysqli_stmt_bind_param($sentencia, 'i', $id);
-
+        // ejecuto la sentencia
         $q = mysqli_stmt_execute($sentencia);
-
+        // guardo el resultado en una variable
         mysqli_stmt_bind_result($sentencia, $nombreArticulo);
+        // recorro el resultado
         mysqli_stmt_fetch($sentencia);
+        // cerrar la sentencia
         mysqli_stmt_close($sentencia);
 
         if (!empty($_GET["confirmar"]) && $_GET["confirmar"] == "true") {
-
+            // si pulsaron el boton de confirmar borro el articulo de la base de datos
             $consulta = "DELETE FROM articulo WHERE id_articulo = ?";
 
             $sentencia = mysqli_prepare($conexion, $consulta);
@@ -41,10 +47,13 @@ if (!empty($_GET["id"]) && !empty($_GET["usuario"])) {
             mysqli_stmt_close($sentencia);
 
             if ($q) {
+                // se borro correctamente asigno el valor a $eliminado com true para mostrar el mensaje de redireccionamiento
                 header("refresh:1;url=articulo_listado.php?usuario=" . $usuario);
                 $eliminado = true;
             } else {
+                // si no se ha podido borrar mostrar el error y redirigir
                 header("refresh:1;url=articulo_listado.php?usuario=" . $usuario);
+                echo '<p>No se ha podido borrar el articulo</p>';
             }
         }
 
@@ -68,8 +77,10 @@ if (!empty($_GET["id"]) && !empty($_GET["usuario"])) {
             <?php
 
             if ($eliminado) {
+                // si fue eliminado mostrar el mensaje de redireccionamiento
                 echo '<p class="mb-4 text-white">Artículo eliminado correctamente redirigiendo...</p>';
             } else {
+                // si aun no fue eliminado mostrar el boton de confirmar y el de cancelar
                 echo '<a href="articulo_eliminar.php?id=' . $id . '&usuario=' . $usuario . '&confirmar=true" class="btn btn-primary me-2">Aceptar</a>';
                 echo '<a href="articulo_listado.php?usuario=' . $usuario . '" class="btn btn-secondary">Cancelar</a>';
             }
