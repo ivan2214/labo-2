e6ed4620ffd4979e25396c797900edcfb02619da
ivan2214 <bongiovanniivan12@gmail.com>
2024-11-09 12:14:07 -0300

    feat: Add "article list" button
    Added button to directly access article list. Improves navigation.

--- Laboratorio-2/TP08-BongiovanniIvanAgustin/php/header.php
+++ Laboratorio-2/TP08-BongiovanniIvanAgustin/php/header.php
@@ -32,9 +32,14 @@ $fechaDeHoy = crearFecha($fecha);
 
 <header class="d-flex align-items-center justify-content-between bg-secondary shadow">
     <section class="p-2">
+        <a class="btn btn-dark" href="articulo_listado.php">
+            Listado de articulos
+        </a>
+    </section>
+    <section class="p-2">
         <h2 class="h5 fw-semibold mb-0"><?= $fechaDeHoy ?></h2>
     </section>
-    <section class="d-flex align-items-center gap-3 px-3 py-2">
+    <section class="d-flex align-items-center gap-3 p-2">
         <h2 class="h5 fw-semibold mb-0"><?= $usuario ?></h2>
         <figure class="rounded-circle overflow-hidden border" style="width: 48px; height: 48px;">
             <img class="img-fluid rounded-circle" src="<?= $ubicacionFoto ?>" alt="Foto de perfil del usuario"
